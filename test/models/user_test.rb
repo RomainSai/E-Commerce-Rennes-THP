d1c4e975de808f55885b94ac127336bdedad7560
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-27 14:41:26 +0200

    model user created with devise

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "User has a first name" do
+    assert true
+  end
+
+  test "User has a last name" do
+    assert true
+  end
+
+  test "User has an email address" do
+    assert true
+  end
+
+  test "User has a valid email address" do
+    assert true
+  end
+
+  test "User has a password" do
+    assert true
+  end
+
+  test "User has a valid password" do
+    assert true
+  end
+
+  test "User is not an admin" do
+    assert true
+  end
+
 end
