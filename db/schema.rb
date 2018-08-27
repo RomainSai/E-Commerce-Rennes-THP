149a78f9959c240991613b770d19c59bdc5b2f8c
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-27 19:47:17 +0200

    devise is functional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_08_27_120431) do
 
   # These are extensions that must be enabled in order to support this database
@@ -27,8 +26,6 @@ ActiveRecord::Schema.define(version: 2018_08_27_120431) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.boolean "admin", default: false
