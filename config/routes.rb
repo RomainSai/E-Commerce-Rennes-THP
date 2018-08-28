232e6fab7fc4c2af1cc84aa73b700632d0bac43f
Ertemelle <phenixime@hotmail.fr>
2018-08-28 19:17:36 +0200

    lalala

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :charges
 
-  root to: 'items#index'
+  root to: 'items#index' as: 'home'
   resources :items
   devise_for :users
 
