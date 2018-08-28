b7c14585b816c332e21165a302cb70bb230584a6
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-28 14:49:06 +0200

    creating the table cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-
+	has_and_belongs_to_many :carts
 	validates :short_description, presence: true
 	validates :long_description, presence: true
 	validates :title, presence: true
