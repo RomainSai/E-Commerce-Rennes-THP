d50ceb59b6b45b86ed460ccc7c0db6075c333dde
Ertemelle <phenixime@hotmail.fr>
2018-08-28 13:21:06 +0200

    shop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.first
   end
 
   def create
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def update
