65f3321831ec6c5b1c03fe9f200ed509e1155a28
Romain <romainsaillour@orange.fr>
2018-08-28 15:17:50 +0200

    essai heroku probleme 406

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_08_28_121751) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.boolean "admin", default: false
