5db158ed9dd267e670f5a6fcf8075159145d1a44
breizzh <remisamsoninvestisseur@gmail.com>
2018-08-29 23:50:16 +0200

    image_url stocké sur ActiveStorage en local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_085823) do
+ActiveRecord::Schema.define(version: 2018_08_29_162735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2018_08_29_085823) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
-    t.string "image_url"
     t.text "short_description"
     t.text "long_description"
     t.datetime "created_at", null: false
