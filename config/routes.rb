7a061529d343956252fc1f85474503e457c33a68
Romain <romainsaillour@orange.fr>
2018-08-29 10:02:10 +0200

    creation controller users et view profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
   get 'session/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :charges
 
-
+  resources :users
   root to: 'items#index'
   get "/about", to: 'static_pages#about'
   get "/contact", to:'static_pages#contact'
