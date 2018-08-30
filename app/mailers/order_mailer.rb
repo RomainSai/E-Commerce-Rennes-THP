215029e9c73d0071fb9dbebecd7531f04e6ac203
christopheleray <christophe.leray1@gmail.com>
2018-08-30 19:31:36 +0200

    order mailer et show index view prix

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -7,9 +7,12 @@ Dotenv.load
     @user = user
     @order = order
     puts params.inspect
-
+    #attachments['filename.jpg'] = File.read("#{Rails.root}/app/assets/images/#{@order.items}")
     p '-' * 20 + 'new_order mailer' + '-' * 20
-    mail(to: @user.email, subject: "Détail de votre commande #{@order.id}")
+    mail(
+      to: @user.email, 
+      subject: "Détail de votre commande #{@order.id}",
+      )
   end
 
   def new_order_admin(order)
@@ -18,7 +21,6 @@ Dotenv.load
     p '-' * 20 + 'new_order_admin mailer' + '-' * 20
     @order = order
     mail(to: admin, subject: "Félicitations, nouvelle commande numéro #{@order.id}")
-
   end
   
 end
